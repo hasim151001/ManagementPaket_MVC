a7ed3fc4f7a44777f40f5e1544561054039a5884
hasim151001 <muhamadmukhasim80@gmail.com>
2023-12-09 13:34:44 +0700

    Add project files.

---
+++ ManagementPaket_MVC/Controllers/JenisPaketController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ManagementPaket_MVC.Models;
+
+namespace ManagementPaket_MVC.Controllers
+{
+    public class JenisPaketController : Controller
+    {
+
+        public IActionResult Index(string pak_id_jenis)
+        {
+            ViewBag.pak_id_jenis = pak_id_jenis;
+
+            return View();
+        }
+        public IActionResult CreateJenisPaket()
+        {
+            return View();
+        }
+        public IActionResult UpdateJenisPaket(string pak_id_jenis)
+        {
+            ViewBag.pak_id_jenis = pak_id_jenis;
+            return View();
+
+        }
+
+
+        public IActionResult DetailJenisPaket(string pak_id_jenis)
+        {
+
+            return View();
+        }
+
+
+    }
+}
---
+++ ManagementPaket_MVC/Controllers/SecurityController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ManagementPaket_MVC.Controllers
+{
+    public class SecurityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
